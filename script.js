8d1c9e97330abada40325ea65c50448a140fc52c
Sol1de <127517324+Sol1de@users.noreply.github.com>
2023-03-12 23:52:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function openCity(cityName, elmnt, color) {
+
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  document.getElementById(cityName).style.display = "block";
+
+  elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
+
+
+function openFlyout() {
+    document.getElementById("flyout").style.visibility = "visible";
+}
+
+function closeFlyout() {
+    document.getElementById("flyout").style.visibility = "hidden";
+}
